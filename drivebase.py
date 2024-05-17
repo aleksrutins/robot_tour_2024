ffd963472be9023f498e61d48937bde91f80af62
Aleks Rūtiņš <aleks@rutins.com>
2024-05-17 11:21:16 +0000

    Using the gyro is really easy. PyBricks is awesome. 'Nuff said.

--- drivebase.py
+++ drivebase.py
@@ -4,23 +4,26 @@ from pybricks.robotics import DriveBase
 
 GRID_SIZE = 500
 
+
 class BBADriveBase(DriveBase):
+
     def __init__(self):
+        self.use_gyro(True)
         self.left_motor = Motor(Port.A)
         self.right_motor = Motor(Port.B)
         super().__init__(self.left_motor, self.right_motor, 55.5, 346)
-    
+
     def init(self):
-        self.straight(GRID_SIZE/2)
+        self.straight(GRID_SIZE / 2)
 
     def forward(self):
         self.straight(GRID_SIZE)
-    
+
     def reverse(self):
         self.straight(-GRID_SIZE)
-    
+
     def left(self):
         self.turn(-90)
-    
+
     def right(self):
         self.turn(90)
